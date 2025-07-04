09e43e1c138f570b6b6f733f4451de4a7c85360e
Luiz <luiz.ribeiro06@aluno.ifce.edu>
2025-07-04 17:09:36 -0300

    Create First View :/

---
+++ ProjetoApple/FirstView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstView.swift
+//  ProjetoApple
+//
+//  Created by found on 04/07/25.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome!")
+        }
+    }
+}
+
+#Preview {
+    FirstView()
+}
