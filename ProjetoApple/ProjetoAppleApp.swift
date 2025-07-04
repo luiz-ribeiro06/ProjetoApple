866c1f2df9a8979ae760f5e6aacba26c868a9115
Luiz <luiz.ribeiro06@aluno.ifce.edu>
2025-07-04 16:44:39 -0300

    Initial Commit

---
+++ ProjetoApple/ProjetoAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoAppleApp.swift
+//  ProjetoApple
+//
+//  Created by found on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
