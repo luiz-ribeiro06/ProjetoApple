83d3258df32d7a1e87cbc0aa26536e097193b4a5
Luiz <luiz.ribeiro06@aluno.ifce.edu>
2025-07-17 17:30:34 -0300

    Comecei a implementar o ProblemView :/ (espero que dê certo)

---
+++ ProjetoApple/ProblemView.swift
@@ -0,0 +1,18 @@
+//
+//  ProblemView.swift
+//  ProjetoApple
+//
+//  Created by found on 17/07/25.
+//
+
+import SwiftUI
+
+struct ProblemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProblemView()
+}
