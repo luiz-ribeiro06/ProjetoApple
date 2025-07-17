272b94c62f8145dec4486ffeade4b93c543dea96
Luiz <luiz.ribeiro06@aluno.ifce.edu>
2025-07-17 17:31:02 -0300

    Pse...

--- ProjetoApple/ProblemView.swift
+++ ProjetoApple/ProblemView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ProblemView: View {
+    @State var problem: Problems
+    
+    @State var selectedAnswer: Int?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack() {
+            VStack(alignment: .leading) {
+                Text(problem.problemStatement)
+                HStack {
+                    Text("#\(problem.subject.rawValue.lowercased())")
+                        .padding(3.5)
+                        .background(.blue)
+                        .cornerRadius(7.5)
+                    if problem.exam != .unspecified {
+                        Text("#\(problem.exam.rawValue.lowercased())")
+                            .padding(3.5)
+                            .background(.blue)
+                            .cornerRadius(5)
+                    }
+                }
+                ForEach(Array(problem.possibleAnswers.enumerated()), id: \.offset) { index, answer in
+                    Button(action: {
+                        // To do: implementar ação ao selecionar uma alternativa
+                    }) {
+                        Text(answer)
+                            .padding(3.5)
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(5)
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ProblemView()
+    ProblemView(problem: Problems(problemStatement: "Quanto é 2 + 2?", subject: .math, exam: .enem, style: .objective, possibleAnswers: ["2", "4", "22", "5"], correctAnswer: 1))
 }
--- ProjetoApple/ProjetoAppleApp.swift
+++ ProjetoApple/ProjetoAppleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ProjetoAppleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MenuView()
         }
     }
 }
