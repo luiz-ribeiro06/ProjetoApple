d68bb80c17d761001c006e2dfd671fa567738ab2
juliaifce <julia.souza08@aluno.ifce.edu.br>
2025-07-22 17:15:46 -0300

    menuview

--- ProjetoApple/MenuView.swift
+++ ProjetoApple/MenuView.swift
@@ -8,46 +8,72 @@
 import SwiftUI
 
 struct MenuView: View {
+    @State private var inputText = ""
+    
+    
     var body: some View {
-        VStack {
+        
+        
+
+        
+        NavigationStack {
             
-            HStack{
-                TextField("Procurar...", text: .constant(""))
-                    .padding(8)
-                    .background(Color(.systemGray5))
-                    .cornerRadius(10)
-                    .frame(maxWidth: 400, alignment: .top)
-                    .frame(maxWidth: .infinity, maxHeight:
-                            .infinity, alignment: .top)
-                    .padding()
-                Image(systemName: "magnifyingglass")
-                    .frame(maxWidth: 60, maxHeight:
-                            354, alignment: .top).font(.largeTitle)
-            }
-                
-            }
+        
             
-            
-        ZStack {
-          
             VStack {
-                Spacer()
-                Rectangle()
-                    .fill(Color.white)
-                    .frame(height: 100)
-                Button(action: {}){
-                    Image(systemName: "house" ).font(.largeTitle)
-                        
-                }
-                
+                TextField("Enter Text", text: $inputText)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                    .onSubmit {
+                        print("Submitted: \(inputText)")
+                       
+                    }
             }
+            Spacer()
+            
+                .toolbar {
+                    ToolbarItem (placement: .bottomBar) {
+                        HStack {
+                            
+                           
+                                Button(action: {}, label: {
+                                    
+                                    VStack (spacing: 4) {
+                                        Image(systemName: "house.fill").font(.title)
+                                        Text("Início").font(.system(size: 16))
+                                    }
+                                })
+                                Button(action: {}, label: {
+                                    VStack (spacing: 4) {
+                                        Image(systemName: "person.3.sequence.fill").font(.title)
+                                        Text("Comunidade").font(.system(size: 16))
+                                    }
+                            })
+                            Button(action: {}, label: {
+                                VStack (spacing: 4) {
+                                    Image(systemName: "hourglass").font(.title)
+                                    Text("Desempenho").font(.system(size: 16))
+                                    
+                                }
+                            })
+                            
+                            Button(action: {}, label: {
+                                VStack (spacing: 4) {
+                                    Image(systemName: "person.circle.fill").font(.title)
+                                    Text("Perfil").font(.system(size: 16))
+                                }
+                                
+                            })
+                            
+                        }
+                    }
+                }
         }
-
-        }
-       
     }
-
-
+}
 #Preview {
     MenuView()
 }
+                        
+                        
+
