fc33103c06d03da51f486e2eb320dc11b85c2b6a
Luiz <luiz.ribeiro06@aluno.ifce.edu>
2025-07-29 17:01:52 -0300

    Commit message (required)

--- ProjetoApple/ContentView.swift
+++ ProjetoApple/ContentView.swift
@@ -116,7 +116,21 @@ struct AddProblemSheet: View {
                         }
                     }
                 } else if style == .objective {
-                    // To do...
+                    ForEach(0..<possibleAnswers.count, id: \.self) { index in
+                        TextField("Opção \(index+1)", text: Binding(
+                            get: { possibleAnswers[index] },
+                            set: { possibleAnswers[index] = $0 }
+                        ))
+                    }
+                    Button("Adicionar Opção") {
+                        possibleAnswers.append("")
+                    }
+
+                    Picker("Resposta Correta", selection: $correctAnswer) {
+                        ForEach(0..<possibleAnswers.count, id: \.self) { index in
+                            Text("Opção \(index + 1)").tag(index)
+                        }
+                    }
                 }
             }
             .navigationTitle("Adicionar Questão")
@@ -149,11 +163,11 @@ struct UpdateProblemSheet: View{
             Form{
                 TextField("Expense Name", text: $problem.problemStatement)
             }
-            .navigationTitle("Edit expense")
+            .navigationTitle("Editar questão")
             .navigationBarTitleDisplayMode(.large)
             .toolbar{
                 ToolbarItemGroup(placement: .topBarLeading){
-                    Button("Done"){ dismiss() }
+                    Button("Pronto"){ dismiss() }
                 }
             }
             
